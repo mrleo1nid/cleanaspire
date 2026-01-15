44b0590204ae76292555c012c1f40c896a8b92fa
Leonid Pershin <lpershin@syntellect.ru>
2026-01-15 15:14:15 +0300

    feat: configure static file MIME types for Blazor WASM

--- src/CleanAspire.WebApp/Program.cs
+++ src/CleanAspire.WebApp/Program.cs
@@ -3,6 +3,7 @@
 // See the LICENSE file in the project root for more information.
 
 using CleanAspire.ClientApp;
+using Microsoft.AspNetCore.StaticFiles;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.AddServiceDefaults();
@@ -16,6 +17,21 @@ builder
 builder.Services.AddCoreServices(builder.Configuration);
 builder.Services.AddHttpClients();
 builder.Services.AddAuthenticationAndLocalization(builder.Configuration);
+
+// Configure static file MIME types for Blazor WASM
+builder.Services.Configure<StaticFileOptions>(options =>
+{
+    var provider = new FileExtensionContentTypeProvider();
+    provider.Mappings[".wasm"] = "application/wasm";
+    provider.Mappings[".blat"] = "application/octet-stream";
+    provider.Mappings[".dat"] = "application/octet-stream";
+    provider.Mappings[".dll"] = "application/octet-stream";
+    provider.Mappings[".pdb"] = "application/octet-stream";
+    provider.Mappings[".json"] = "application/json";
+    provider.Mappings[".js"] = "application/javascript";
+    provider.Mappings[".mjs"] = "application/javascript";
+    options.ContentTypeProvider = provider;
+});
 
 var app = builder.Build();
 
@@ -34,6 +50,26 @@ else
 app.UseHttpsRedirection();
 
 app.UseAntiforgery();
+
+// Configure static files with proper MIME types for Blazor WASM
+var staticFileOptions = new StaticFileOptions
+{
+    ContentTypeProvider = new FileExtensionContentTypeProvider
+    {
+        Mappings =
+        {
+            [".wasm"] = "application/wasm",
+            [".blat"] = "application/octet-stream",
+            [".dat"] = "application/octet-stream",
+            [".dll"] = "application/octet-stream",
+            [".pdb"] = "application/octet-stream",
+            [".json"] = "application/json",
+            [".js"] = "application/javascript",
+            [".mjs"] = "application/javascript",
+        },
+    },
+};
+app.UseStaticFiles(staticFileOptions);
 
 app.MapStaticAssets();
 app.MapRazorComponents<CleanAspire.WebApp.Components.App>()
